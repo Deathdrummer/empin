5ee9304b2e28e7661dc064573515ca94b9f2d3ec
Dmitriy <deathdrumer@yandex.ru>
2022-08-31 09:22:50 +0300

    чат - право на отправку сообщений в чат договора

--- resources/views/site/section/contracts/render/chat/list.blade.php
+++ resources/views/site/section/contracts/render/chat/list.blade.php
@@ -6,34 +6,36 @@
 			@endforeach
 		</div>
 		
-		<div class="mt2rem">
-			<div class="row align-items-end">
-				<div class="col">
-					{{-- <x-textarea
-						rows="5"
-						group="normal"
-						class="w100"
-						/> --}}
-					
-					<div
-						id="chatMessageBlock"
-						class="color-gray-600 border-all border-gray-300 border-radius-5px minh4rem maxh15rem scrollblock p1rem"
-						contenteditable
-						>
+		@cando('contract-col-chat-can-sending:site')
+			<div class="mt2rem">
+				<div class="row align-items-end">
+					<div class="col">
+						{{-- <x-textarea
+							rows="5"
+							group="normal"
+							class="w100"
+							/> --}}
 						
+						<div
+							id="chatMessageBlock"
+							class="color-gray-600 border-all border-gray-300 border-radius-5px minh4rem maxh15rem scrollblock p1rem"
+							contenteditable
+							>
+							
+						</div>
+					</div>
+					<div class="col-auto">
+						<x-button
+							id="chatSendMesageBtn"
+							group="large"
+							variant="blue"
+							disabled
+							title="Отправить сообщение"
+							action="chatSendMesage:{{$contractId}}"
+							><i class="fa-solid fa-paper-plane"></i></x-button>
 					</div>
 				</div>
-				<div class="col-auto">
-					<x-button
-						id="chatSendMesageBtn"
-						group="large"
-						variant="blue"
-						disabled
-						title="Отправить сообщение"
-						action="chatSendMesage:{{$contractId}}"
-						><i class="fa-solid fa-paper-plane"></i></x-button>
-				</div>
 			</div>
-		</div>
+		@endcando
 	</div>
 @endif
