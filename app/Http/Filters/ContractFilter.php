c7768028b8eea8e0399bc8c90d2e84f1fa082cde
Dmitriy <deathdrumer@yandex.ru>
2022-09-18 12:34:33 +0300

    добавление полей в модалку общая информация + поиск по полям, которые идут из настроек

--- app/Http/Filters/ContractFilter.php
+++ app/Http/Filters/ContractFilter.php
@@ -1,9 +1,12 @@
 <?php namespace App\Http\Filters;
 
 use App\Http\Filters\Base\AbstractFilter;
+use App\Traits\Settingable;
 use Illuminate\Database\Eloquent\Builder;
 
 class ContractFilter extends AbstractFilter {
+	use Settingable;
+	
     public const ID = 'id';
     public const CONTRACT_ID = 'contract_id';
     public const ARCHIVE = 'archive';
@@ -57,14 +60,58 @@ class ContractFilter extends AbstractFilter {
 	
 	
 	public function search(Builder $builder, $value) {
-		$builder->where(function (Builder $query) use($value) {
+		$settings = $this->getSettings([[
+				'setting'	=> 'contract-customers:customers',
+				'key'		=> 'id',
+				'value'		=> 'name'
+			], [
+				'setting'	=> 'contract-types:types',
+				'key'		=> 'id',
+				'value'		=> 'title'
+			], [
+				'setting'	=> 'contract-contractors:contractors',
+				'key'		=> 'id',
+				'value'		=> 'name'
+			]
+		]);
+		
+		$customersMatch = $this->_subSearch($settings['customers'], $value);
+		$typesMatch = $this->_subSearch($settings['types'], $value);
+		$contractorsMatch = $this->_subSearch($settings['contractors'], $value);
+		
+		$builder->where(function (Builder $query) use($value, $customersMatch, $typesMatch, $contractorsMatch) {
 			$query->orWhere('object_number', 'like', '%'.$value.'%');
 			$query->orWhere('title', 'like', '%'.$value.'%');
 			$query->orWhere('applicant', 'like', '%'.$value.'%');
 			$query->orWhere('titul', 'like', '%'.$value.'%');
 			$query->orWhere('contract', 'like', '%'.$value.'%');
+			$query->orWhere('locality', 'like', '%'.$value.'%');
+			
+			if ($customersMatch) $query->orWhereIn('customer', $customersMatch);
+			if ($typesMatch) $query->orWhereIn('type', $typesMatch);
+			if ($contractorsMatch) $query->orWhereIn('contractor', $contractorsMatch);
 		});
     }
+	
+	
+	
+	
+	
+	
+	/** Поиск по подстроке
+	 * @param array  $data
+	 * @param mixed  $value
+	 * @return array
+	 */
+	private function _subSearch($data, $value): array {
+		$keysToSearch = [];
+		foreach ($data as $key => $str) {
+			if (mb_stripos($str, $value) !== false) {
+				$keysToSearch[] = $key;
+			}
+		}
+		return $keysToSearch;
+	}
 	
 	
 }
