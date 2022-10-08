8ec3715a6fb137749d19dfb51d85446b43b4730c
Dmitriy <deathdrumer@yandex.ru>
2022-10-08 17:44:18 +0300

    тест 1

--- resources/views/site/section/contracts/render/list.blade.php
+++ resources/views/site/section/contracts/render/list.blade.php
@@ -29,7 +29,8 @@
 								class="sort{{$sortField == 'title' ? ' sort-'.$sortOrder : ''}} vertical-center"
 								onclick="$.sorting(this, 'title')"
 								noscroll
-								><strong class="fz10px lh90 d-block text-center">Название / заявитель</strong></td>
+								><strong class="fz10px lh90 d-block text-center">Название / 
+								заявитель</strong></td>
 						@endif
 						
 						@if(auth('site')->user()->can('contract-col-applicant:site') && (empty($userColums) || in_array('applicant', $userColums)))
