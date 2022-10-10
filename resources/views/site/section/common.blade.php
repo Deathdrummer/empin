a6f8cd825a73f77e09c10e3fa5227df24b591a46
Dmitriy <deathdrumer@yandex.ru>
2022-10-10 15:54:48 +0300

    новая таблица 4

--- resources/views/site/section/common.blade.php
+++ resources/views/site/section/common.blade.php
@@ -4,26 +4,96 @@
 		>
 		
 		
+		<x-horisontal space="2rem" scroll="false">
+			<x-horisontal.item>
+				<x-table>
+					<x-table.head>
+						<x-table.tr>
+							<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
+							<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
+							<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
+						</x-table.tr>
+					</x-table.head>
+					<x-table.body>
+						@for ($i = 0; $i < 100; $i++)
+						<x-table.tr class="h5rem">
+							<x-table.td><p>body 1</p></x-table.td>
+							<x-table.td><p>body 2</p></x-table.td>
+							<x-table.td><p>body 3</p></x-table.td>
+						</x-table.tr>
+						@endfor
+					</x-table.body>
+					<x-table.foot></x-table.foot>
+				</x-table>
+			</x-horisontal.item>
+			
+			<x-horisontal.item>
+				<x-table>
+					<x-table.head>
+						<x-table.tr>
+							<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
+							<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
+							<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
+						</x-table.tr>
+					</x-table.head>
+					<x-table.body>
+						@for ($i = 0; $i < 100; $i++)
+						<x-table.tr class="h5rem">
+							<x-table.td><p>body 1</p></x-table.td>
+							<x-table.td><p>body 2</p></x-table.td>
+							<x-table.td><p>body 3</p></x-table.td>
+						</x-table.tr>
+						@endfor
+					</x-table.body>
+					<x-table.foot></x-table.foot>
+				</x-table>
+			</x-horisontal.item>
+			
+			<x-horisontal.item>
+				<x-table>
+					<x-table.head>
+						<x-table.tr>
+							<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
+							<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
+							<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
+						</x-table.tr>
+					</x-table.head>
+					<x-table.body>
+						@for ($i = 0; $i < 100; $i++)
+						<x-table.tr class="h5rem">
+							<x-table.td><p>body 1</p></x-table.td>
+							<x-table.td><p>body 2</p></x-table.td>
+							<x-table.td><p>body 3</p></x-table.td>
+						</x-table.tr>
+						@endfor
+					</x-table.body>
+					<x-table.foot></x-table.foot>
+				</x-table>
+			</x-horisontal.item>
+			
+			<x-horisontal.item>
+				<x-table>
+					<x-table.head>
+						<x-table.tr>
+							<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
+							<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
+							<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
+						</x-table.tr>
+					</x-table.head>
+					<x-table.body>
+						@for ($i = 0; $i < 100; $i++)
+						<x-table.tr class="h5rem">
+							<x-table.td><p>body 1</p></x-table.td>
+							<x-table.td><p>body 2</p></x-table.td>
+							<x-table.td><p>body 3</p></x-table.td>
+						</x-table.tr>
+						@endfor
+					</x-table.body>
+					<x-table.foot></x-table.foot>
+				</x-table>
+			</x-horisontal.item>
+		</x-horisontal>
 		
-		<x-table>
-			<x-table.head>
-				<x-table.tr>
-					<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
-					<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
-					<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
-				</x-table.tr>
-			</x-table.head>
-			<x-table.body>
-				@for ($i = 0; $i < 100; $i++)
-				<x-table.tr class="h5rem">
-					<x-table.td><p>body 1</p></x-table.td>
-					<x-table.td><p>body 2</p></x-table.td>
-					<x-table.td><p>body 3</p></x-table.td>
-				</x-table.tr>
-				@endfor
-			</x-table.body>
-			<x-table.foot></x-table.foot>
-		</x-table>
 		
 		
 		
