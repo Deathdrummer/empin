807d9da0b8995bc6224cb222c655b064c550b31d
Dmitriy <deathdrumer@yandex.ru>
2022-10-11 18:50:09 +0300

    новая таблица 9

--- resources/views/site/section/common.blade.php
+++ resources/views/site/section/common.blade.php
@@ -96,32 +96,29 @@
 		</x-horisontal>
 		
 		
-		<div class="h3rem"></div>
-		
-		
-		<x-table>
-			<x-table.head>
-				<x-table.tr>
-					<x-table.td style="width: 250px;"><p>thead 1</p></x-table.td>
-					<x-table.td style="width: 300px;"><p>thead 2</p></x-table.td>
-					<x-table.td style="width: 130px;"><p>thead 3</p></x-table.td>
-				</x-table.tr>
-			</x-table.head>
-			<x-table.body class="h30rem">
-				@for ($i = 0; $i < 10; $i++)
-				<x-table.tr class="h5rem">
-					<x-table.td><p>body 1</p></x-table.td>
-					<x-table.td><p>body 2</p></x-table.td>
-					<x-table.td><p>body 3</p></x-table.td>
-				</x-table.tr>
-				@endfor
-			</x-table.body>
-			<x-table.foot></x-table.foot>
-		</x-table>
-		
-		
-		
 		
 	
 	</x-card>
 </section>
+
+
+
+
+<script type="module">
+	
+	$('[ddrtablebody]').on('mouseover', function() {
+		$(this).addClass('ddrtablebody-hovered');
+		$('[ddrtablebody]').not(this).removeClass('ddrtablebody-hovered');
+	});
+	
+	
+	
+	
+	$('[ddrtablebody]').scroll(function() {
+		if ($(this).hasClass('ddrtablebody-hovered') == false) return;
+		let scrTop = $(this).scrollTop();
+		$('[ddrtablebody]').not('.ddrtablebody-hovered').scrollTop(scrTop);
+	});
+	
+	
+</script>
