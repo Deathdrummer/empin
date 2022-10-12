b661b9a7d524ca78f30ffa7caab4157ed76abcea
Dmitriy <deathdrumer@yandex.ru>
2022-10-12 11:55:02 +0300

    блочная таблица тест 3

--- resources/views/site/section/common.blade.php
+++ resources/views/site/section/common.blade.php
@@ -16,7 +16,7 @@
 						</x-table.tr>
 					</x-table.head>
 					<x-table.body class="h30rem">
-						@for ($i = 0; $i < 100; $i++)
+						@for ($i = 0; $i < 1000; $i++)
 						<x-table.tr class="h5rem">
 							<x-table.td><p>body 1</p></x-table.td>
 							<x-table.td><p>body 2</p></x-table.td>
@@ -38,7 +38,7 @@
 						</x-table.tr>
 					</x-table.head>
 					<x-table.body class="h30rem">
-						@for ($i = 0; $i < 100; $i++)
+						@for ($i = 0; $i < 1000; $i++)
 						<x-table.tr class="h5rem">
 							<x-table.td><p>body 1</p></x-table.td>
 							<x-table.td><p>body 2</p></x-table.td>
@@ -60,7 +60,7 @@
 						</x-table.tr>
 					</x-table.head>
 					<x-table.body class="h30rem">
-						@for ($i = 0; $i < 100; $i++)
+						@for ($i = 0; $i < 1000; $i++)
 						<x-table.tr class="h5rem">
 							<x-table.td><p>body 1</p></x-table.td>
 							<x-table.td><p>body 2</p></x-table.td>
@@ -82,7 +82,7 @@
 						</x-table.tr>
 					</x-table.head>
 					<x-table.body class="h30rem">
-						@for ($i = 0; $i < 100; $i++)
+						@for ($i = 0; $i < 1000; $i++)
 						<x-table.tr class="h5rem">
 							<x-table.td><p>body 1</p></x-table.td>
 							<x-table.td><p>body 2</p></x-table.td>
