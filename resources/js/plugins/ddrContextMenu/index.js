eff0ad0042461d56d30de97009dc9459588d989d
Dmitriy <deathdrumer@yandex.ru>
2022-10-22 17:41:40 +0300

    контекстное меню 4

--- resources/js/plugins/ddrContextMenu/index.js
+++ resources/js/plugins/ddrContextMenu/index.js
@@ -7,7 +7,7 @@ $(document).on('contextmenu', '[contextmenu]', function(e) {
 	
 	let menuHtml = '';
 		menuHtml += '<ul class="context noselect">';
-		menuHtml +=		'<li>';
+		menuHtml +=		'<li class="parent">';
 		menuHtml += 		'<i class="icon fa-fw fa-solid fa-triangle-exclamation"></i>';
 		menuHtml += 		'<p>Пункт меню 1</p>';
 		menuHtml += 		'<i class="f fa-solid fa-chevron-right"></i>';
@@ -31,9 +31,24 @@ $(document).on('contextmenu', '[contextmenu]', function(e) {
 		menuHtml += 		'<i class="icon fa-fw fa-solid fa-download"></i>';
 		menuHtml += 		'<p>Пункт меню 2</p>';
 		menuHtml += 	'</li>';
-		menuHtml += 	'<li>';
-		menuHtml += 		'<i class="icon fa-fw fa-brands fa-slack"></i>';
+		menuHtml +=		'<li class="parent">';
+		menuHtml += 		'<i class="icon fa-fw fa-solid fa-triangle-exclamation"></i>';
 		menuHtml += 		'<p>Пункт меню 3</p>';
+		menuHtml += 		'<i class="f fa-solid fa-chevron-right"></i>';
+		menuHtml += 		'<ul class="context sub">';
+		menuHtml += 			'<li>';
+		menuHtml += 				'<i class="icon fa-fw fa-solid fa-download"></i>';
+		menuHtml += 				'<p>Подпункт меню 1</p>';
+		menuHtml += 			'</li>';
+		menuHtml += 			'<li>';
+		menuHtml += 				'<i class="icon fa-fw fa-solid fa-magnifying-glass"></i>';
+		menuHtml += 				'<p>Подпункт меню 2</p>';
+		menuHtml += 			'</li>';
+		menuHtml += 			'<li>';
+		menuHtml += 				'<i class="icon fa-fw fa-brands fa-discord"></i>';
+		menuHtml += 				'<p>Подпункт меню 3</p>';
+		menuHtml += 			'</li>';
+		menuHtml += 		'</ul>';
 		menuHtml += 	'</li>';
 		menuHtml += 	'<li class="divline"></li>';
 		menuHtml += 	'<li class="hilight">';
@@ -127,15 +142,29 @@ $(document).on('contextmenu', '[contextmenu]', function(e) {
 	
 
 	
-	$context.on("mousedown touchstart", "li:not(.nope)", function(e) {
+	$context.on("mousedown mouseup touchstart touchend", "li:not(.nope):not(.parent)", function(e) {
+		
+		e.stopPropagation();
+		if (e.which !== 1) return;
+		
+		if (hasIn(['mousedown', 'touchstart'], e.type) !== false) {
+			console.log('d');
+			$(this).addClass("active");
+		} else if (hasIn(['mouseup', 'touchend'], e.type) !== false) {
+			$(this).removeClass("active");
+			console.log('u');
+		}
+		
+		
+		
 		console.log('1');
-		if (e.which === 1) {
+		/*if (e.which === 1) {
 			let $item = $(this);
 			$item.removeClass("active");
 			setTimeout(function() {
 				$item.addClass("active");
 			}, 10);
-		}
+		}*/
 	});
 });
 
