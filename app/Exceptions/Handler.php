ef05bd1cbc2a21d1d74ace24935604df29a3d2e8
Deathdrummer <deathdrumer@yandex.ru>
2022-12-07 12:13:43 +0300

    exceptions handler fix 3 --UPLOAD

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,8 +52,7 @@ class Handler extends ExceptionHandler {
 		
 		Log::error("[{$code}] \"{$message}\" of file: {$file}:{$line} on line: {$line}");
 		
-		return false;
-		//parent::report($e);
+		parent::report($e);
 		//return;
 	}
 	
