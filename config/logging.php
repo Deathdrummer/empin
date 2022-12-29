e1314ed72218787fd44adc7219ef0b43e0f7e1ec
Deathdrummer <deathdrumer@yandex.ru>
2022-12-29 20:41:02 +0300

    добавление логирования в телеграм  --UPLOAD

--- config/logging.php
+++ config/logging.php
@@ -48,6 +48,11 @@ return [
     */
 
     'channels' => [
+		'telegram' => [
+			'driver' => 'custom',
+			'via'    => Logger\TelegramLogger::class,
+			'level'  => 'debug',
+		],
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
