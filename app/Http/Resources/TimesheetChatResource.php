6d86a1781c6e23f585285b56f839c623c5236513
Dmitriy <deathdrumer@yandex.ru>
2025-10-25 17:40:13 +0300

    fix: TimesheetChatResource null user check

--- app/Http/Resources/TimesheetChatResource.php
+++ app/Http/Resources/TimesheetChatResource.php
@@ -10,13 +10,15 @@ class TimesheetChatResource extends JsonResource {
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request) {
+        $currentUser = auth('site')->user();
+
         return [
             'id'        => $this->id,
             'day'       => $this->day,
             'message'   => $this->message,
             'created_at'=> $this->created_at->translatedFormat('d F Y г. в H:i'),
             'updated_at'=> $this->updated_at,
-			'self'		=> $this->from_id === auth('site')->user()->staff_id,
+			'self'		=> $currentUser ? ($this->from_id === $currentUser->staff_id) : false,
 			'from' 	=> $this->profile ? [
 							'id'		=> $this->from_id,
 							'full_name' => $this->profile->full_name,
