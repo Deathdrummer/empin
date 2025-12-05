1a4574fef1ce8dba7c0e5e8f1345298b6a301dfa
Dmitriy <deathdrumer@yandex.ru>
2025-12-05 20:22:36 +0300

    праава для сотрудников fix

--- resources/views/admin/section/personal/render/users_new/user_card.blade.php
+++ resources/views/admin/section/personal/render/users_new/user_card.blade.php
@@ -107,11 +107,13 @@
 				onclick="$.usersNewSendEmail(this, {{$id}});"
 				title="{{!is_null($registred['temporary_password']) ? 'Выслать доступ сотруднику' : 'Выслать доступ повторно'}}"
 				></i>
-			<i
-				class="fa-solid fa-fw fa-right-to-bracket ml10px fz18px color-blue-600 color-hovered-darken pointer"
-				onclick="$.usersNewLoginAs(this, {{$id}});"
-				title="Войти от имени сотрудника"
-				></i>
+			@if(auth('admin')->user()?->is_main_admin && Str::contains(url()->current(), '/admin'))
+				<i
+					class="fa-solid fa-fw fa-right-to-bracket ml10px fz18px color-blue-600 color-hovered-darken pointer"
+					onclick="$.usersNewLoginAs(this, {{$id}});"
+					title="Войти от имени сотрудника"
+					></i>
+			@endif
 		</div>
 	@endif
 </div>
