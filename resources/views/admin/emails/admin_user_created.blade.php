161fb82d05a21d0d6146e46ec46b906b9a15f22d
Dmitriy <deathdrumer@yandex.ru>
2025-12-18 13:13:06 +0300

    исправление грамматических ошибок

--- resources/views/admin/emails/admin_user_created.blade.php
+++ resources/views/admin/emails/admin_user_created.blade.php
@@ -4,7 +4,7 @@
 	<p>Ваш пароль: {{$adminUser['password']}}</p>
 	<p><a href="{{url('/admin')}}">Войти в систему</a></p>
 @else
-	<h2>Воостановление доступа</h2>
+	<h2>Восстановление доступа</h2>
 	<h4>Аккаунт: <strong>{{$adminUser['email']}}</strong></h4>
 	<p>Пароль можно восстановить <a href="{{url('/admin')}}"><strong>здесь</strong></a> кликнув на ссылку: <strong>"забыли пароль?"</strong></p>
 @endisset
