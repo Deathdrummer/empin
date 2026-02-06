2318484c41b4d21dc61af1f67d5a801b8ccb0f3a
Dmitriy <deathdrumer@yandex.ru>
2026-02-06 15:41:35 +0300

    разделение прав для редактирования ячеек инфо. таблицы fix 3

--- resources/views/site/section/contracts/render/row_common.blade.php
+++ resources/views/site/section/contracts/render/row_common.blade.php
@@ -55,7 +55,7 @@
 	@endif
 	
 	@if($column == 'title' && auth('site')->user()->can('contract-col-title:site'))
-		<x-table.td class="breakword h-start" commonlist :contextedit="(auth('site')->user()->can('contract-can-edit-cell:site') || auth('site')->user()->can('edit:title:site')) ? $id.','.$column.',1' : false" style="background-color: {{$selected_color.'75' ?? ''}};">
+		<x-table.td class="breakword h-start" commonlist :contextedit="(auth('site')->user()->can('contract-can-edit-cell::site') || auth('site')->user()->can('edit:title:site')) ? $id.','.$column.',1' : false" style="background-color: {{$selected_color.'75' ?? ''}};">
 			<div class="scrollblock-hidden maxh4rem-6px pr3px">
 				<p class="fz12px lh100 mt2px mb2px" edittedplace="-">{{Str::of($title ?? '-')->limit(60, '...')}}</p>
 			</div>
